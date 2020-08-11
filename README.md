c997d69524f5956d4b1f655a1835b21d0ec2f3b2
Vlada Antonchenko <vladajka16@gmail.com>
2020-08-11 02:43:11 -0400

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Team_Profile
+
+As a manager
+I want to generate a webpage that displays my team's basic info
+so that I have quick access to emails and GitHub profiles
+```
+There are also unit tests to help you build the classes necessary.
+
+It is recommended that you follow this workflow:
+
+1. Run tests
+2. Create or update classes to pass a single test case
+3. Repeat
+
+🎗 Remember, you can run the tests at any time with `npm run test`
+
+It is recommended that you start with a directory structure that looks like this:
+
+```
+lib/           // classes and helper code
+output/        // rendered output
+templates/     // HTML template(s)
+test/          // jest tests
+  Employee.test.js
+  Engineer.test.js
+  Intern.test.js
+  Manager.test.js
+app.js         // Runs the application
+```
+
---
+++ app.js
@@ -0,0 +1,94 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+
+
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const inquirer = require("inquirer");
+const render = require("./lib/htmlRenderer.js");
+
+const employees = [];
+
+
+
+function addMember() {
+    inquirer.prompt([{
+        message: "Enter team member's name",
+        name: "name"
+    },
+    {
+        type: "list",
+        message: "Select team member's role",
+        choices: [
+            "Engineer",
+            "Intern",
+            "Manager"
+        ],
+        name: "role"
+    },
+    {
+        message: "Enter team member's id",
+        name: "id"
+    },
+    {
+        message: "Enter team member's email address",
+        name: "email"
+    }])
+        .then(function ({ name, role, id, email }) {
+            let roleInfo = "";
+            if (role === "Engineer") {
+                roleInfo = "GitHub username";
+            } else if (role === "Intern") {
+                roleInfo = "school name";
+            } else {
+                roleInfo = "office phone number";
+            }
+            inquirer.prompt([{
+                message: `Enter team member's ${roleInfo}`,
+                name: "roleInfo"
+            },
+            {
+                type: "list",
+                message: "Would you like to add more team members?",
+                choices: [
+                    "yes",
+                    "no"
+                ],
+                name: "moreMembers"
+            }])
+                .then(function ({ roleInfo, moreMembers }) {
+                    let newMember;
+                    if (role === "Engineer") {
+                        newMember = new Engineer(name, id, email, roleInfo);
+                    } else if (role === "Intern") {
+                        newMember = new Intern(name, id, email, roleInfo);
+                    } else {
+                        newMember = new Manager(name, id, email, roleInfo);
+                    }
+                    employees.push(newMember);
+                    if (moreMembers === "yes") {
+                        addMember();
+                    } else {
+                        finishHtml();
+                    }
+                });
+
+        });
+}
+
+function finishHtml() {
+    const template = render(employees);
+    fs.writeFile("./output/team.html", template, function (err) {
+        if (err) {
+            console.log(err);
+        };
+    });
+    console.log("'Congrats Your Pofile Team Is Generated'\n Plz!, checkout in Output folder ");
+}
+addMember();
